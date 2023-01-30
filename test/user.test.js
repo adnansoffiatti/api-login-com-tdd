f16201f449b51506fcc1455d6726689cfdc86312
adnansoffiatti <79457983+adnansoffiatti@users.noreply.github.com>
2023-01-30 12:00:33 -0300

    Teste - Teste de e-mail repetido

--- test/user.test.js
+++ test/user.test.js
@@ -32,4 +32,32 @@ describe("Cadastro de usuário", () => {
                 fail(err)
             });
     })
+
+    test("Deve impedir que um usuário se cadastre com um e-mail repedito", () => {
+
+        let time = Date.now();
+        let email = `${time}@gmail.com`;
+        let user = {name: "Adnan", email, password: "123456"};
+
+        return request.post("/user")
+            .send(user)
+            .then(res => {
+
+                expect(res.statusCode).toEqual(200);
+                expect(res.body.email).toEqual(email);
+
+                return request.post("/user")
+                    .send(user)
+                    .then(res => {
+
+                        expect(res.statusCode).toEqual(400);
+                        expect(res.body.error).toEqual("E-mail já cadastrado");
+
+                    }).catch(err => {
+                        fail(err);
+                    });
+            }).catch(err => {
+                fail(err);
+            });
+    });
 });
